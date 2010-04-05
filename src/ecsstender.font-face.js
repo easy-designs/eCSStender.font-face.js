232e37a1e5ec7e78e55088f93e30cd3edf60ab14
aarongustafson <aaron@easy-designs.net>
2010-04-05 19:35:49 -0400

    updated the extension to not block on large font files. Should keep the UI responsive.

--- src/ecsstender.font-face.js
+++ src/ecsstender.font-face.js
@@ -2,7 +2,7 @@
 Function:       eCSStender.font-face.js
 Author:         Aaron Gustafson (aaron at easy-designs dot net)
 Creation Date:  2009-10-09
-Version:        0.1
+Version:        0.2
 Homepage:       http://github.com/easy-designs/eCSStender.font-face.js
 License:        MIT License 
 Note:           If you change or improve on this script, please let us know by
@@ -27,6 +27,18 @@ Note:           If you change or improve on this script, please let us know by
   e.onComplete(function(){
     var 
     
+    // strings
+    EMPTY = '',
+    SPACE = ' ',
+    SEMIC = '; ',
+    PRCNT = '%',
+
+    // patterns
+    regexp_pattern =  /url\(\s*['"]?([^'"\s)]*)['"]?\s*\)/,
+    font_pattern   = '@font-face { % } ',
+    url_pattern    = "url('%')",
+    format_pattern = "format('%')",
+    
     // supported formats
     formats = {
       eot: FALSE,
@@ -34,79 +46,122 @@ Note:           If you change or improve on this script, please let us know by
       otf: 'opentype',
       svg: 'svg'
     },
-    format,
     svg_id = e.methods.svg_id || FALSE,
     
     // font parsing vars
     fonts  = e.fonts,
     count  = fonts.length,
-    i, font, src, url,
-    
-    // styles
-    contents, props,
-    styles         = '',
-    font_pattern   = '@font-face { % } ',
-    url_pattern    = "url('%')",
-    format_pattern = "format('%')",
+    files  = [],
+    exist  = [],
+    i, j,
     
     // XMLHttpRequest
-    __xhr  = NULL;
+    checking = FALSE,
+    __xhr    = new XHR();
+    __xhr.onreadystatechange = checkFiles;
+    
+    init();
         
-    // go through the fonts in the system
-    for ( i=0; i<count; i++ )
+    function init()
     {
-      contents = '';
-      font     = fonts[i];
-      src      = font.src.replace( /url\(\s*['"]?([^'"\s)]*)['"]?\s*\)/, '$1' );
-      // add the local block
-      font.src = [ "local('" + font['font-family'] + "')" ];
-      // now test each version
-      for ( format in formats )
+      var font, src, format;
+      for ( i=j=0; i<count; i++ )
       {
-        if ( e.isInheritedProperty( formats, format ) ){ continue; }
-        file = src + '.' + format;
-        if ( fileExists( file ) )
+        font = fonts[i];
+        src  = font.src.replace( regexp_pattern, '$1' );
+        // now test each version
+        for ( format in formats )
         {
-          if ( format == 'eot' )
-          {
-            contents += 'src: ' + url_pattern.replace( '%', file ) + '; ';
-          }
-          else
-          {
-            if ( format == 'svg' &&
-                 svg_id != '' )
-            {
-              file += '#' + svg_id;
-            }
-            font.src.push( url_pattern.replace( '%', file ) +
-                           ( formats[format] != FALSE ? ' ' + format_pattern.replace( '%', formats[format] )
-                                                      : '' ) );
-          }
+          if ( e.isInheritedProperty( formats, format ) ){ continue; }
+          files[j++] = src + '.' + format;
         }
       }
-      font.src = font.src.join(', ');
-      // write in the properties
-      for ( prop in font )
-      {
-        if ( e.isInheritedProperty( font, prop ) ){ continue; }
-        contents += prop + ': ' + font[prop] + '; ';
-      }
-      styles += font_pattern.replace( '%', contents );
+      i=0;
+      checkFiles();
     }
-    e.embedCSS( styles );
-    
-    // utils
-    function fileExists( uri )
+    function checkFiles()
     {
-      if ( uri == NULL ){ return FALSE; }
-      if ( __xhr == NULL ){ __xhr = new XHR(); }
-      __xhr.open( 'GET', uri, FALSE );
-      __xhr.send( NULL );
-      var status = NULL;
-      do {
-        if ( __xhr.readyState == 4 ) { status = __xhr.status; }
-      } while ( status == NULL )
-      return status == 200 ? TRUE : FALSE;
+      var file, status;
+      if ( ! checking )
+      {
+        if ( file = files[--j] )
+        {
+          checking = TRUE;
+          __xhr.open( 'HEAD', file, FALSE );
+          __xhr.send( NULL );
+        }
+        else
+        {
+          files = exist.join(' ');
+          writeCSS();
+        }
+      }
+      else
+      {
+        if ( __xhr.readyState == 4 )
+        {
+          checking = FALSE;
+          status = __xhr.status;
+          if ( status >= 200 && 
+               ( status < 300 || status == 304 ) )
+          {
+            exist[i++] = files[j];
+          }
+          checkFiles();
+        }
+      }
+    }
+    function writeCSS()
+    {
+      var 
+      contents, prop,
+      font, src,
+      file,
+      format,
+      styles = EMPTY;
+
+      // go through the fonts in the system
+      for ( i=0; i<count; i++ )
+      {
+        contents = [];
+        font     = fonts[i];
+        src      = font.src.replace( regexp_pattern, '$1' );
+        // add the local block
+        font.src = [ "local('" + font['font-family'] + "')" ];
+        // now test each version
+        for ( format in formats )
+        {
+          if ( e.isInheritedProperty( formats, format ) ){ continue; }
+          file = src + '.' + format;
+          if ( files.indexOf( file ) > -1 )
+          {
+            if ( format == 'eot' )
+            {
+              contents.push('src: ' + url_pattern.replace( PRCNT, file ) + SEMIC);
+            }
+            else
+            {
+              if ( format == 'svg' &&
+                   svg_id != EMPTY )
+              {
+                file += '#' + svg_id;
+              }
+              font.src.push( url_pattern.replace( PRCNT, file ) +
+                             ( formats[format] != FALSE ? SPACE + format_pattern.replace( PRCNT, formats[format] )
+                                                        : EMPTY ) );
+            }
+          }
+        }
+        font.src = font.src.join(', ');
+        // write in the properties
+        for ( prop in font )
+        {
+          if ( e.isInheritedProperty( font, prop ) ){ continue; }
+          contents.push( prop + ': ' + font[prop] + SEMIC );
+        }
+        styles += font_pattern.replace( PRCNT, contents.join('') );
+      }
+      e.embedCSS( styles );
     }
     function XHR()
     {
